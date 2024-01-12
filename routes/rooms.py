3e82f055ae768dfdaaad7d5a566533e6e78985d0
youngji Jang <btg1631@naver.com>
2024-01-12 12:34:21 +0900

    youngji find_rooms

--- routes/rooms.py
+++ routes/rooms.py
@@ -15,13 +15,14 @@ templates = Jinja2Templates(directory="templates/")
 
 # 방 찾기
 @router.get("/find_rooms", response_class=HTMLResponse) # 펑션 호출 방식
-async def find_rooms_get(request:Request):
-    return templates.TemplateResponse(name="room/find_rooms.html", context={'request':request})
+async def find_rooms(request:Request):
+    search_dict = {'room_type': 'none', 'room_size': 'none', 'search': ''}
+    return templates.TemplateResponse(name="room/find_rooms.html", context={'request':request, 'search_dict':search_dict})
 
 # 방 찾기(검색 클릭시 선택한 옵션과 검색어에 대한 내용이 다음 페이지로 넘어가야함..) 
 @router.post("/find_rooms")
-async def find_rooms_post(request:Request):
+async def find_rooms(request:Request):
     search_dict = dict(await request.form())
     print(search_dict)
-    return templates.TemplateResponse(name="room/find_rooms.html", context={'request':request})
+    return templates.TemplateResponse(name="room/find_rooms.html", context={'request':request, 'search_dict':search_dict})
 
