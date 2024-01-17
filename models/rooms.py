9ab1ae44b5c9867c5603c90b345f4ea49a469def
youngji Jang <btg1631@naver.com>
2024-01-17 09:45:18 +0900

    push

--- models/rooms.py
+++ models/rooms.py
@@ -6,12 +6,18 @@ from pydantic import BaseModel, EmailStr
 # 개발자 실수로 들어가는 field 제한
 class ROOM_DATA(Document):
     room_brand: Optional[str] = None
-    room_local: Optional[str] = None
-    room_title: Optional[str] = None
+    room_local: Optional[str] = None    
     room_image: Optional[str] = None
-    room_room_type: Optional[str] = None
-    room_room_size : Optional[str] = None
-    room_text : Optional[str] = None
+    room_title: Optional[str] = None
+    room_type: Optional[str] = None
+    room_any: Optional[str] = None
+    room_size : Optional[str] = None
+    room_layout : Optional[str] = None
+    room_option : Optional[str] = None
+    room_default_option : Optional[str] = None
+    room_price : Optional[str] = None
+    room_discounted_price : Optional[str] = None
+    room_note : Optional[str] = None
   
     class Settings:
         name = "ROOM_DATA"
