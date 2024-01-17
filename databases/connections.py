0d450f4a9af5cb12c25ceb3f9279ccee286ea201
scvgong <gongmyeongyun@gmail.com>
2024-01-17 15:38:37 +0900

    change faq

--- databases/connections.py
+++ databases/connections.py
@@ -5,7 +5,7 @@ from models.rooms import ROOM_DATA
 from models.reviews import REVIEW_DATA
 from models.enters_users import ENTER_USER_DATA
 from models.enters_rooms import ENTER_ROOM_DATA
-from models.faqs import FAQ
+from models.qnas import QNA
 from motor.motor_asyncio import AsyncIOMotorClient
 from pydantic import BaseModel
 
@@ -18,7 +18,7 @@ class Settings(BaseSettings):
     async def initialize_database(self):
         client = AsyncIOMotorClient(self.DATABASE_URL)
         await init_beanie(database=client.get_default_database(),
-                          document_models=[USER_DATA, ROOM_DATA, REVIEW_DATA,ENTER_USER_DATA,ENTER_ROOM_DATA,FAQ])
+                          document_models=[USER_DATA, ROOM_DATA, REVIEW_DATA,ENTER_USER_DATA,ENTER_ROOM_DATA,QNA])
     
     class Config:
         env_file = ".env"
--- models/qnas.py
+++ models/qnas.py
@@ -4,10 +4,10 @@ from beanie import Document, Link
 from pydantic import BaseModel, EmailStr
 
 # 개발자 실수로 들어가는 field 제한
-class FAQ(Document):
+class QNA(Document):
     title: Optional[str] = None
     sellist : Optional[str] = None
     text : Optional[str] = None
 
     class Settings:
-        name = "FAQ"
+        name = "QNA"
